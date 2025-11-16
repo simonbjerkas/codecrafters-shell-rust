62907fb429a706705604fb9a603d97386f25f7f5
simonbjerkas <simon.bjerkas10@gmail.com>
2025-11-16 23:54:11 +0100

    type command added

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,34 @@
 use std::io::{self, Write};
 
 enum Commands {
-    Exit,
-    Echo { args: Vec<String> },
+    Exit { args: i32 },
+    Echo { args: String },
+    Type { args: String },
 }
+
+const COMMANDS: [&str; 3] = ["exit", "echo", "type"];
 
 impl Commands {
     fn from_input(input: &str) -> Option<Commands> {
-        let (command_type, args) = input.trim().split_once(' ').unwrap_or(("", ""));
-        let command_type = command_type.to_lowercase();
+        let mut input = input.trim().split_whitespace();
+        let command_type = input.next().unwrap_or("").to_lowercase();
 
         match command_type.as_str() {
-            "exit" => Some(Commands::Exit),
+            "exit" => {
+                let args = input.next();
+                match args {
+                    Some(arg) => match arg.parse::<i32>() {
+                        Ok(num) => Some(Commands::Exit { args: num }),
+                        Err(_) => None,
+                    },
+                    None => panic!("Missing argument for exit command"),
+                }
+            }
             "echo" => Some(Commands::Echo {
-                args: vec![args.to_string()],
+                args: input.collect(),
+            }),
+            "type" => Some(Commands::Type {
+                args: input.collect(),
             }),
             _ => None,
         }
@@ -30,8 +45,21 @@ fn main() {
 
         let command = Commands::from_input(&input);
         match command {
-            Some(Commands::Exit) => break,
-            Some(Commands::Echo { args }) => println!("{}", args.join(" ")),
+            Some(Commands::Exit { args }) => {
+                if args > 1 {
+                    panic!("Ughh")
+                } else {
+                    std::process::exit(args);
+                }
+            }
+            Some(Commands::Echo { args }) => println!("{}", args),
+            Some(Commands::Type { args }) => {
+                if COMMANDS.contains(&args.as_str()) {
+                    println!("{} is a shell builtin", args);
+                } else {
+                    println!("{}: not found", args);
+                }
+            }
             None => println!("{}: command not found", input.trim()),
         }
     }
