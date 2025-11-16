c65d221a1faafeed45854419c2dbf84b46e26a56
simonbjerkas <simon.bjerkas10@gmail.com>
2025-11-16 20:07:01 +0100

    waiting for command with loop

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,21 @@
 use std::io::{self, Write};
 
 fn main() {
-    let mut command = String::new();
-    print!("$ ");
-    io::stdin().read_line(&mut command).unwrap();
-    println!("{}: command not found", command);
-    io::stdout().flush().unwrap();
+    loop {
+        let mut command = String::new();
+        print!("$ ");
+        io::stdout().flush().unwrap();
+
+        if io::stdin().read_line(&mut command).unwrap() == 0 {
+            println!("Exiting...");
+            break;
+        }
+
+        let command = command.trim();
+        if command.is_empty() {
+            continue;
+        }
+
+        println!("{}: command not found", command);
+    }
 }
